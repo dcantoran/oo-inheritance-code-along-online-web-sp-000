fdae41426e88e85aed5b20a851877f285e9943f7
David Cantoran <davidcantoran36@gmail.com>
2019-12-20 18:41:29 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,8 @@
 class Vehicle
-
+  attr_accessor :wheel_size, :wheel_number
+  
+  def intialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end 
 end
